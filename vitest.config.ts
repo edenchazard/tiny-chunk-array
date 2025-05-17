09f7b8e5b5d68ead438c3a19caea9769e24b95a3
Charley Reeson <neon.fortytwo@gmail.com>
2025-05-17 11:26:31 +0100

    coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,15 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{js,ts}'],
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
   },
 });
